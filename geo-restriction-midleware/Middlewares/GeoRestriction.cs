a547919a8548a72fe6f114572fb20df6be3f9178
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2025-09-30 06:07:50 +0300

    Added the maxmind db instance in the Resources/Geo folder + Added the restricted countries in the appsettings.json

--- geo-restriction-midleware/Middlewares/GeoRestriction.cs
+++ geo-restriction-midleware/Middlewares/GeoRestriction.cs
@@ -1,9 +1,27 @@
 using System.Net;
+using MaxMind.GeoIP2;
 
 namespace geo_restriction_midleware.Middlewares;
 
-public class GeoRestriction(ILogger<GeoRestriction> logger, RequestDelegate next)
+public class GeoRestriction()
 {
+    private readonly ILogger<GeoRestriction> _logger;
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _config;
+    private readonly DatabaseReader _dbReader;
+    private readonly HashSet<string> _blockedCountries;
+
+    public GeoRestriction(ILogger<GeoRestriction> logger, RequestDelegate next, IConfiguration config)
+    {
+        _logger = logger;
+        _next = next;
+        _config = config;
+        _dbReader = new DatabaseReader("/Resources/Geo/GeoLite2-City.mmdb");
+        _blockedCountries = config.GetSection("GeoRestriction:BlockedCountries")
+            .Get<string[]>()?
+            .ToHashSet() ?? new HashSet<string>() ;
+
+    }
 
 
     public async Task InvokeAsync(HttpContext context)
@@ -15,8 +33,8 @@ public class GeoRestriction(ILogger<GeoRestriction> logger, RequestDelegate next
                 "Sorry we could not  identify your geographical location, try again later");
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
         }
-        logger.LogInformation("IpAddress {ipAddress}", ipAddress);
-        await next(context);
+        _logger.LogInformation("IpAddress {ipAddress}", ipAddress);
+        await _next(context);
 
     }
 
